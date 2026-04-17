9b96bae93c11d544cae25c2cc1a708a3483a4c19
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-04-17 15:14:21 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/home.html')
-def contact(request):
-    return HttpResponse("Contato")
-def about(request): #Reecebemos a requisição do cliente
-    return HttpResponse("Sobre") #Retornamos a resposta
-    
