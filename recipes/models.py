1cd94e2256aa3f420417a1fc1354f4cebe60ce7d
Álvaro  Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-05-17 14:13:15 -0300

    Iniciando a criação do primeiro model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Library(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True) 
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.
+    author = models
+
+
